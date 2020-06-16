1f673802fadd0c7489a6cd3d2bbcfe0d36fa0914
yura <wa7er14@gmail.com>
2020-06-16 19:54:59 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-console.log('hello mate academy!');
+const images = document.querySelectorAll('.list-item__link');
+const largeImage = document.querySelector('#largeImg');
+
+function handler(ev) {
+  const currentImg = ev.currentTarget.firstElementChild;
+
+  largeImage.src = currentImg.src.replace('-thumb', '').replace('jpeg', 'png');
+  ev.preventDefault();
+}
+
+images.forEach(image => image.addEventListener('click', handler));
